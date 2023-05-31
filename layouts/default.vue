d5173fc19bfc5a22bca1493456f76f04afed45a4
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-05-31 23:24:05 +0330

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+	<q-layout view="hHh lpR fFf" class="flex column">
+		<Header />
+
+		<q-page-container class="q-my-xl q-px-xl">
+			<slot />
+		</q-page-container>
+
+		<Footer />
+	</q-layout>
+</template>
+
+<style lang="scss" scoped>
+.q-layout {
+	min-height: 100%;
+	min-height: 100dvh;
+}
+
+.q-page-container {
+	flex: 1 1 auto;
+}
+</style>
