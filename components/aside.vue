492d9823011066bed7b88cf2c718aaa7b05a2786
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-01 01:43:47 +0330

    feat: create aside component

---
+++ components/aside.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { fasHashtag } from '@quasar/extras/fontawesome-v5';
+</script>
+<template>
+	<aside :class="$style.aside">
+		<section :class="$style.popularTags">
+			<h6 class="q-pb-md flex row no-wrap justify-between items-center">
+				<span class="shrink">Popular Tags</span>
+				<MedalIcon />
+			</h6>
+
+			<div class="flex row wrap q-gutter-sm">
+				<q-btn
+					rounded
+					no-caps
+					unelevated
+					size="md"
+					color="dark"
+					text-color="white"
+					label="implementations"
+					:icon="fasHashtag"
+					class="text-weight-regular"
+				/>
+			</div>
+		</section>
+	</aside>
+</template>
+
+<style lang="scss" module>
+.aside {
+	& > section {
+		background-color: #33394f;
+		padding: 1rem;
+		border-radius: 10px;
+	} // & > section
+} // .aside
+.popularTags {
+	:global {
+		.q-icon {
+			font-size: 0.85rem;
+			margin-right: 6px;
+		} // .q-icon
+	} // :global
+} // .popularTags
+</style>
---
+++ components/icons/MedalIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-1em h-1em">
+		<path
+			d="M12 15C15.728 15 18.75 12.09 18.75 8.5C18.75 4.91 15.728 2 12 2C8.272 2 5.25 4.91 5.25 8.5C5.25 12.09 8.272 15 12 15Z"
+			stroke="#D1CAFF"
+			stroke-width="1.5"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M7.52001 13.5198L7.51001 20.8998C7.51001 21.7998 8.14001 22.2398 8.92001 21.8698L11.6 20.5998C11.82 20.4898 12.19 20.4898 12.41 20.5998L15.1 21.8698C15.87 22.2298 16.51 21.7998 16.51 20.8998V13.3398"
+			stroke="#D1CAFF"
+			stroke-width="1.5"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</svg>
+</template>
