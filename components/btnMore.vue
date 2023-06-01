0e88a4fdfea6ac26e8cc5b5b2c48e76999b356d6
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-02 01:27:43 +0330

    feat: create btn more component

---
+++ components/btnMore.vue
@@ -0,0 +1,10 @@
+<template>
+	<q-btn flat unelevated rounded label="Read more..." class="btn-more" :class="$style.btnMore" />
+</template>
+
+<style lang="scss" module>
+.btnMore {
+	background-color: #d9d9d9;
+	color: #33394f;
+}
+</style>
