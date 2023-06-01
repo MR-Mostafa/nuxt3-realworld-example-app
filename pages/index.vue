2c89393c8005dd1cc47d0464dd84bbc2eaec2ad1
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-02 01:32:00 +0330

    chore: update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-	<h3>Index Page</h3>
+	<div class="flex row q-gutter-x-lg">
+		<main class="col">
+			<Article />
+			<Article />
+			<Article />
+			<Article />
+			<Article />
+			<Article />
+
+			<div class="q-pt-lg">
+				<Pagination />
+			</div>
+		</main>
+
+		<Aside class="col-4 q-pl-lg" />
+	</div>
 </template>
