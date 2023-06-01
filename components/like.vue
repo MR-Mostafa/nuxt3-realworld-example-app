8ed49ddad49f6874d0591aaf0e2d294070bc7ff3
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-02 01:28:27 +0330

    feat: create like component

---
+++ components/like.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { farHeart } from '@quasar/extras/fontawesome-v5';
+</script>
+
+<template>
+	<q-btn :icon="farHeart" label="320" :class="$style.btnLike" />
+</template>
+
+<style lang="scss" module>
+.btnLike {
+	background-color: #d1caff;
+	color: #252d3a;
+}
+
+.activeLike {
+	background-color: #fc728b;
+	color: #fff;
+} // .activeLike
+</style>
