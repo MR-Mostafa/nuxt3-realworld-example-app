61a6b4e1b25efbf7c5f4bc6cd835a7030bbe8e8e
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-03 21:06:43 +0330

    feat: create follow component

---
+++ components/follow.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { farUser } from '@quasar/extras/fontawesome-v5';
+</script>
+
+<template>
+	<q-btn :icon="farUser" label="Follow User Name" :class="[$style.followBtn, $style.activeFollow]" no-caps />
+</template>
+
+<style lang="scss" module>
+.followBtn {
+	background-color: #d1caff;
+	color: #252d3a;
+}
+
+.activeFollow {
+	background-color: #9ab78d;
+	color: #202433;
+} // .activeFollow
+</style>
