079c5b65f7546dc542b5f4c08d2f93d0d8e95ea3
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-09 01:33:26 +0330

    fix: add static "Token" string

--- composables/useToken.ts
+++ composables/useToken.ts
@@ -1,11 +1,11 @@
-import { useCookie, type CookieOptions } from 'nuxt/app';
+import { useCookie, useRuntimeConfig, type CookieOptions } from 'nuxt/app';
 
 export const useToken = <T = string | null | undefined>(options: CookieOptions<T> = {}) => {
 	const config = useRuntimeConfig();
 	const token = useCookie(config.public.cookieName, options);
 
 	const get = () => {
-		return token.value;
+		return `Token ${token.value}`;
 	};
 
 	const set = (value: T) => {
