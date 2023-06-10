4c202c60bd90af14afc8239f079bb6862902cd52
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-10 18:34:33 +0330

    feat: add timeout for get user data

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ import { useAPI } from '~/composables';
 import { authState } from '~/store';
 import { User } from '~/types';
 
-const { data } = await useAPI<User>('/user');
+const { data } = await useAPI<User>('/user', { timeout: 4000 });
 const auth = authState();
 
 auth.set(data.value || undefined);
