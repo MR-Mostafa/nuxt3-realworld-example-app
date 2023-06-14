b02d8d62834647c9f0ccdd7d3f938551621f4b51
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-14 14:32:08 +0330

    fix: hydration node mismatch in auth page when user logged in

--- app.vue
+++ app.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-import { useAPI } from '~/composables';
-import { authState } from '~/store';
-import { User } from '~/types';
-
-const { data } = await useAPI<User>('/user', { timeout: 4000 });
-const auth = authState();
-
-auth.set(data.value || undefined);
-</script>
-
 <template>
 	<NuxtLayout>
 		<NuxtPage />
--- middleware/checkToken.global.ts
+++ middleware/checkToken.global.ts
@@ -1,8 +1,18 @@
 import { defineNuxtRouteMiddleware } from '#imports';
-import { useToken } from '~/composables';
+import { useAPI, useToken } from '~/composables';
+import { authState } from '~/store';
+import { User } from '~/types';
 
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware(async () => {
 	const token = useToken();
 
-	if (!token) return '';
+	if (!token) return;
+
+	const auth = authState();
+
+	if (auth.get.value) return;
+
+	const { data } = await useAPI<User>('/user', { timeout: 4000 });
+
+	auth.set(data.value || undefined);
 });
