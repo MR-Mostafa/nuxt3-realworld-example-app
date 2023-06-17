227a964282e3027fa84c908a6d17516b2b11f217
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-17 13:03:54 +0330

    refactor: add props

--- components/avatar.vue
+++ components/avatar.vue
@@ -1,12 +1,25 @@
+<script setup lang="ts">
+import { computed } from '#imports';
+
+const props = defineProps<{ avatarSrc: string; username: string; date: string }>();
+
+const formatedDate = computed(() => {
+	const date = new Date(props.date);
+	return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+});
+</script>
+
 <template>
 	<div class="avatar-wrapper flex row items-center justify-start" :class="$style.avatarWrapper">
-		<q-avatar icon="img:/sample/demo-avatar.png" size="44px" />
+		<q-avatar size="44px">
+			<q-img :src="props.avatarSrc" :ratio="1" width="44" height="44" :alt="props.username" placeholder-src="/no-image.jpeg" />
+		</q-avatar>
 
 		<p class="q-pl-sm">
 			<span class="block text-weight-medium">
-				<NuxtLink to="/" class="text-white text-body2">Ebayyou</NuxtLink>
+				<NuxtLink :to="`/@${props.username}`" class="text-white text-body2">{{ props.username }}</NuxtLink>
 			</span>
-			<span class="block date text-caption">6h ago</span>
+			<time class="block date text-caption" :datetime="props.date">{{ formatedDate }}</time>
 		</p>
 	</div>
 </template>
@@ -15,7 +28,7 @@
 .avatarWrapper {
 	:global {
 		.q-avatar {
-			border-radius: 5px;
+			border-radius: 50%;
 
 			.q-icon {
 				width: 100%;
