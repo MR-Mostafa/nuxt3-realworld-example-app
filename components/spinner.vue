c56b1e9a6c4694f15cfcb0f04633c1588dc08b57
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-17 20:52:19 +0330

    feat: create spinner component

---
+++ components/spinner.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+	if (process.client) {
+		window.scrollTo({ behavior: 'smooth', top: 0 });
+	}
+});
+</script>
+
+<template>
+	<q-inner-loading :showing="true" label="Please wait..." :class="$style.spinner" transition-duration="0" />
+</template>
+
+<style lang="scss" module>
+.spinner {
+	padding: 2rem 0;
+	position: static;
+	background-color: transparent;
+}
+</style>
