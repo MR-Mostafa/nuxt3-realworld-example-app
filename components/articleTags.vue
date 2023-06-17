466ee146e1ae5ea66f07b51587b45a7e3bedce1c
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-06-17 20:49:44 +0330

    refactor: add props

--- components/articleTags.vue
+++ components/articleTags.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
 import { fasHashtag } from '@quasar/extras/fontawesome-v5';
+
+const props = defineProps<{ tagList: string[] }>();
 </script>
 
 <template>
 	<q-list tag="ul" dense :class="$style.tagsWrapper" class="flex inline row no-wrap text-body2 items-center justify-end">
-		<q-item dense tag="li">
-			<q-btn dense flat no-caps :icon="fasHashtag" label="tag 1" />
-		</q-item>
-		<q-item dense tag="li">
-			<q-btn dense flat no-caps :icon="fasHashtag" label="tag 2" />
-		</q-item>
-		<q-item dense tag="li">
-			<q-btn dense flat no-caps :icon="fasHashtag" label="tag 3" />
-		</q-item>
-		<q-item dense tag="li">
-			<q-btn dense flat no-caps :icon="fasHashtag" label="tag 4" />
-		</q-item>
-		<q-item dense tag="li">
-			<q-btn dense flat no-caps :icon="fasHashtag" label="tag 5" />
+		<q-item v-for="(item, index) in props.tagList" :key="index" dense tag="li">
+			<q-btn dense flat no-caps :icon="fasHashtag" :label="item" :to="`/?tag=${item}`" />
 		</q-item>
 	</q-list>
 </template>
